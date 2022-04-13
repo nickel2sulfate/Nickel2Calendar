a8a6088715eef43a482d73354cd10ddb9752ab4c
Nicole Solt <nsulfate@gmail.com>
2022-04-12 20:19:53 -0400

    Added Base Code

---
+++ main.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw, ImageFont
+
+tSunday = input("What Time are you streaming on Sunday?: ") 
+
+with Image.open("assets/cal.png") as cal: 
+
+    textLayer = Image.new("RGBA", cal.size, (255,255,255,0))
+
+    fnt = ImageFont.truetype("assets/Manrope-Light.ttf",100)
+
+    brush = ImageDraw.Draw(textLayer)
+
+    brush.text((285,360), tSunday, font=fnt, fill=(225,255,255,255))
+
+    output = Image.alpha_composite(cal,textLayer)
+
+    output.show()
+
+
+
+
+
+
