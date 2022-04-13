d01fc51a61e39de4e41c7464a1f68976d034081a
Tres Sims <tres.sims95@gmail.com>
2022-04-12 21:24:15 -0400

    Iterate through all days of the week to get times

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from PIL import Image, ImageDraw, ImageFont
 
-tSunday = input("What Time are you streaming on Sunday?: ") 
+color = (225, 100, 100, 255)
+# TODO: This process should be repeated for the "command" section
+times = []
+for i in range(1, 8):
+    # TODO: This could be improved to say the actual days of the week...
+    newTime = input(f"What time do you want to stream on day {i} of the week?: ")
+    times.append(newTime)
 
-with Image.open("assets/cal.png") as cal: 
+with Image.open("assets/cal.png") as cal:
 
-    textLayer = Image.new("RGBA", cal.size, (255,255,255,0))
+    textLayer = Image.new("RGBA", cal.size, (255, 255, 255, 0))
 
-    fnt = ImageFont.truetype("assets/lucon.ttf",100)
+    fnt = ImageFont.truetype("assets/lucon.ttf", 100)
 
     brush = ImageDraw.Draw(textLayer)
 
-    brush.text((285,360), tSunday, font=fnt, fill=(225,255,255,255))
+    # TODO: This process should be repeated for the "command" section
+    baseLocation = [285, 360]
+    offset = [0, 137]
+    i = 0
+    for time in times:
+        thisOffset = (
+                offset[0] * i + baseLocation[0],
+                offset[1] * i + baseLocation[1]
+            )
+        brush.text(
+                thisOffset,
+                time, 
+                font=fnt, 
+                fill=color
+            )
+        i = i + 1
 
-    output = Image.alpha_composite(cal,textLayer)
+    output = Image.alpha_composite(cal, textLayer)
 
     output.show()
-
-
-
-
-
-
